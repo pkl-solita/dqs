a6c097e0c9add2af4a7d62e1015587f5fa01657d
Paul Klinkenberg <pklinken.development@gmail.com>
2026-05-13 10:01:29 +0200

    Update config

--- vite.config.ts
+++ vite.config.ts
@@ -3,7 +3,13 @@ import react from '@vitejs/plugin-react'
 import { VitePWA } from 'vite-plugin-pwa'
 
 const repositoryName = process.env.GITHUB_REPOSITORY?.split('/')[1] ?? ''
-const base = process.env.GITHUB_ACTIONS && repositoryName ? `/${repositoryName}/` : '/'
+const isUserOrOrgSiteRepo = repositoryName.toLowerCase().endsWith('.github.io')
+const base =
+  process.env.GITHUB_ACTIONS && repositoryName
+    ? isUserOrOrgSiteRepo
+      ? '/'
+      : `/${repositoryName}/`
+    : '/'
 
 // https://vite.dev/config/
 export default defineConfig({
